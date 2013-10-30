8ad4811b1f8f1d3b7f600f24902528a69f109eca
FrostyTehSnowman <travis@synergysquad.com>
2013-10-29 21:19:52 -0400

    Added Some Classes and a Library
    Added classes: Server, Constants
    Added Libraries: netty-4.0.11.Final

---
+++ src/com/eoc/Server.java
@@ -0,0 +1,20 @@
+/**
+ * The Game Server
+ */
+package com.eoc;
+
+/**
+ * @author Travis
+ *
+ */
+public class Server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
